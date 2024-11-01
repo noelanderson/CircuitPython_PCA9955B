ee841896819e6dbff4d689bf471caef5ba00189b
Noel Anderson <noel_anderson@outlook.com>
2024-11-01 16:41:46 -0700

    Initial version

---
+++ setup.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2024 Noel Anderson
+#
+# SPDX-License-Identifier: MIT
+
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    # Community Bundle Information
+    name="circuitpython-pca9955b",
+    use_scm_version={
+        # This is needed for the PyPI version munging in the Github Actions release.yml
+        "git_describe_command": "git describe --tags --long",
+        "local_scheme": "no-local-version",
+    },
+    setup_requires=["setuptools_scm"],
+    description="CircuitPython helper library for the NXP 16-Channel I²C-Bus Constant-Current LED Driver",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    # The project's main homepage.
+    url="https://github.com/NoelAnderson/CircuitPython_PCA9955B.git",
+    # Author details
+    author="Noel Anderson",
+    author_email="",  # TODO: Add your email here
+    install_requires=[
+        "Adafruit-Blinka",
+        "adafruit-circuitpython-busdevice",
+        "no",
+    ],
+    # Choose your license
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Hardware",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    # What does your project relate to?
+    keywords="adafruit blinka circuitpython micropython pca9955b LED Driver, I2C, PWM "
+             "controller",
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # TODO: IF LIBRARY FILES ARE A PACKAGE FOLDER,
+    #       CHANGE `py_modules=['...']` TO `packages=['...']`
+    py_modules=["pca9955b"],
+)

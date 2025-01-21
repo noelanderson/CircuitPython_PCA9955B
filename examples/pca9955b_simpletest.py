a59961c62949d7e0cbfff295b27f8be49c4569b9
Noel Anderson <noel_anderson@outlook.com>
2025-01-21 10:51:13 -0800

    simple test example

--- examples/pca9955b_simpletest.py
+++ examples/pca9955b_simpletest.py
@@ -1,4 +1,33 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2025 Noel Anderson
 #
 # SPDX-License-Identifier: Unlicense
+
+import time
+
+import board
+import busio
+
+from pca9955b import PCA9955, LedChannel
+
+# Create I2C bus.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+
+ledDriver = PCA9955(i2c, address=0x3F, oe_pin=board.GP10, reset_pin=board.GP11)
+ledDriver.reset()
+ledDriver.output_enable = True
+
+
+ledDriver.gain = 0xFF
+ledDriver.brightness = 0x7F  # 50% brightness
+
+ledDriver.channels[0].output_state = LedChannel.ON
+time.sleep(5)
+ledDriver.channels[0].output_state = LedChannel.OFF
+
+time.sleep(5)
+
+ledDriver.channels[0].output_state = LedChannel.PWM
+for i in range(255):
+    ledDriver.channels[0].brightness = i
+    time.sleep(0.02)
